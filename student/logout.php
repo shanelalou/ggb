f63fcd96efc89668136dd4bc9bc0819ae8e61e74
iamsuperrhex <johnrexell@gmail.com>
2013-09-29 10:59:56 +0800

    Revert "yehey"
    This reverts commit 156750ed440f2fdde6d7d1506bb0bd33da889919.

--- student/logout.php
+++
@@ -1,7 +0,0 @@
-<?php include '../config.php';?>
-<?php
-		if(isset($_SESSION['student'])){
-			session_destroy();
-			header('Location: ../index.html');
-		}
-?>
--- student/verify.php
+++
@@ -1,6 +0,0 @@
-<?php include '../config.php';?>
-<?php
-	mysql_query("update account set status='1' where verification_code='".filt($_GET['code'])."' and username='".filt($_GET['verify'])."'");
-	$_SESSION['student'] = $_GET['verify'];
-	header("Location:../student");
-?>
